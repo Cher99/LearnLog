a2c357b8c8b97085b34bdd0fc37817a3d42194b0
Cher99 <Cher_99@outlook.com>
2019-09-08 21:02:15 +0800

    first hello_rospy:DONE

---
+++ ROS/hello_rospy/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/cheney/LearnLog/ROS/hello_rospy/src
+# Build directory: /home/cheney/LearnLog/ROS/hello_rospy/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(beginner_tutorials)
---
+++ ROS/hello_rospy/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/cheney/LearnLog/ROS/hello_rospy/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/cheney/LearnLog/ROS/hello_rospy/build"
+export PYTHONPATH="/opt/ros/kinetic/lib/python2.7/dist-packages:/home/cheney/NAO_QI/naoqi-sdk/naoqi-sdk-2.1.4.13-linux64/pynaoqi-python2.7-2.1.4.13-linux64"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/cheney/LearnLog/ROS/hello_rospy/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/cheney/LearnLog/ROS/hello_rospy/src:$ROS_PACKAGE_PATH"
